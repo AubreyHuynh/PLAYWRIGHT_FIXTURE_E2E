9ed1d9e0dadb534c6f6060436e54c892d31b878b
HuynhMinhNghia <huynhminhnghia.hmn99@gmail.com>
2026-06-03 06:53:41 +0700

    feat: add locator component library barrel export

--- src/locators/index.ts
+++ src/locators/index.ts
@@ -1,31 +1,22 @@
-// Base
-export { BaseComponent } from "./baseComponent";
-
-// Display
-export { Card } from "./display/card";
-export { Table } from "./display/table";
-export { Tag } from "./display/tag";
-
-// Form
-export { Button } from "./form/button";
-export { Checkbox } from "./form/checkbox";
-export { DatePicker } from "./form/datePicker";
-export { DropdownList } from "./form/dropdownList";
-export { Input } from "./form/input";
-export { InputNumber } from "./form/inputNumber";
-export { Radio } from "./form/radio";
-export { Switch } from "./form/switch";
-export { Upload } from "./form/upload";
-
-// Navigation
-export { Pagination } from "./navigation/pagination";
-export { Steps } from "./navigation/steps";
-export { Tabs } from "./navigation/tabs";
-
-// Popup
-export { Alert } from "./popup/alert";
-export { Dialog } from "./popup/dialog";
-export { Sheet } from "./popup/sheet";
-export { Spinner } from "./popup/spinner";
-export { Toast } from "./popup/toast";
-export { Tooltip } from "./popup/tooltip";
+export { BaseComponent } from './baseComponent'
+export { Input } from './form/input'
+export { InputNumber } from './form/inputNumber'
+export { Button } from './form/button'
+export { Checkbox } from './form/checkbox'
+export { Radio } from './form/radio'
+export { DropdownList } from './form/dropdownList'
+export { DatePicker } from './form/datePicker'
+export { Switch } from './form/switch'
+export { Upload } from './form/upload'
+export { Card } from './display/card'
+export { Table } from './display/table'
+export { Tag } from './display/tag'
+export { Tabs } from './navigation/tabs'
+export { Pagination } from './navigation/pagination'
+export { Steps } from './navigation/steps'
+export { Alert } from './popup/alert'
+export { Dialog } from './popup/dialog'
+export { Sheet } from './popup/sheet'
+export { Spinner } from './popup/spinner'
+export { Toast } from './popup/toast'
+export { Tooltip } from './popup/tooltip'
